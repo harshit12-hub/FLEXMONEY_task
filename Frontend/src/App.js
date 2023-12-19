e912f9a73929fa9900966e47f23b4158d92df2c7
harshit12-hub <115970946+harshit12-hub@users.noreply.github.com>
2023-12-19 18:22:18 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
 <div className="flex items-center justify-center min-h-screen bg-slate-300">
       <div>
-        <h1 className='text-2xl font-sans font-bold text-center mb-5 '><span className=' text-orange-400'>APKA KOTHA </span>Registration Form </h1>
+        <h1 className='text-2xl font-sans font-bold text-center mb-5 '>Yoga Registration Form </h1>
        <div className='grid grid-cols-2 gap-6'>
        <RegistrationForm />
         <PaymentForm/> 
